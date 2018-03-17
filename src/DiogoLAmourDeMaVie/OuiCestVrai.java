7786294c6a6cb1b914a46fb0914f7bd181620af2
Micaela <Micaela@10.91.129.184>
2018-03-17 11:47:35 +0100

    Constructeur

---
+++ src/DiogoLAmourDeMaVie/OuiCestVrai.java
@@ -0,0 +1,13 @@
+package DiogoLAmourDeMaVie;
+
+public class OuiCestVrai {
+
+
+		
+		private String gitFromScratch; 
+		
+		public OuiCestVrai(String parameter) {
+			this.gitFromScratch = parameter;
+		}
+
+}
